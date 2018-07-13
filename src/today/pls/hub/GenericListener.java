86ec56d8100b5be23598dc612113090d73006ec7
raidandfade <business@gocode.it>
2018-07-13 06:29:50 -0400

    Server Selection, Player Inventory, and more
    = Major code/import cleanup
    + Set player inventory to default on join
    + Prevent item movement in inventory
        + Create server selection menu with (temporary) static server list
        + Player Lobby show/hide others
        + Empty handbook
    = Convert player arraylists to uuid arraylists

--- src/today/pls/hub/GenericListener.java
+++ src/today/pls/hub/GenericListener.java
@@ -1,35 +1,84 @@
 package today.pls.hub;
 
-import fr.neatmonster.nocheatplus.NCPAPIProvider;
-import fr.neatmonster.nocheatplus.checks.Check;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
 import fr.neatmonster.nocheatplus.checks.CheckType;
-import fr.neatmonster.nocheatplus.components.NoCheatPlusAPI;
 import fr.neatmonster.nocheatplus.hooks.NCPExemptionManager;
 import net.minecraft.server.v1_12_R1.Position;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityCombustEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.event.player.PlayerToggleSneakEvent;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.player.*;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
+import static today.pls.hub.Utils.*;
 
 public class GenericListener implements Listener {
 
-    private Position PKP1 = new Position(-2,107,-13);
-    private Position PKP2 = new Position(2,112,-9);
-    private ArrayList<Player> flyingPlayers = new ArrayList<>();
-    private ArrayList<Player> respawnedPlayers = new ArrayList<>();
+    private static final int BOOK_SLOT = 0, TOGGLE_PLAYER_SLOT = 8, SERVER_SELECT_SLOT = 4;
+
+    private HubPlugin pl;
+
+    private static Position PKP1 = new Position(-2,107,-13);
+    private static Position PKP2 = new Position(2,112,-9);
+
+    private static ArrayList<UUID> flyingPlayers = new ArrayList<>();
+    private static ArrayList<UUID> respawnedPlayers = new ArrayList<>();
+
+    private static ArrayList<UUID> playersHidingOthers = new ArrayList<>();
+
+    private static Inventory playerInv, serverSelectInv;
+    private static ItemStack serverSelectItem, hidePlayersItem, showPlayersItem, infoBook;
+
+    static{
+        serverSelectItem = itemStackWithName(Material.COMPASS,"§c§lServer Selection");
+        hidePlayersItem = itemStackWithName(Material.TORCH, "§4§lHide Players");
+        showPlayersItem = itemStackWithName(Material.LEVER, "§4§lShow Players");
+
+        infoBook = new ItemStack(Material.WRITTEN_BOOK);
+        infoBook.addUnsafeEnchantment(Enchantment.THORNS, 1337);
+        BookMeta m = (BookMeta) infoBook.getItemMeta();
+        m.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        m.setTitle("PLSCraft Handbook");
+        m.setAuthor("RaidAndFade");
+        m.setGeneration(BookMeta.Generation.ORIGINAL);
+        m.addPage("Welcome to PLSCraft");
+        infoBook.setItemMeta(m);
+
+
+        playerInv = Bukkit.createInventory(null, 36, "§c§lServer Selection");
+        playerInv.setItem(BOOK_SLOT, infoBook);
+        playerInv.setItem(SERVER_SELECT_SLOT, serverSelectItem);
+        playerInv.setItem(TOGGLE_PLAYER_SLOT, hidePlayersItem);
+
+        serverSelectInv = Bukkit.createInventory(null, 9); //simplistic and hardcoded for now
+        serverSelectInv.setItem(0, itemStackWithNameAndLore(Material.DIAMOND_PICKAXE, "§1§lServer: §2§lSURVIVAL", "§3Mine & Craft!"));
+        serverSelectInv.setItem(4, itemStackWithNameAndLore(Material.GRASS, "§1§lServer: §6§lSKYBLOCK","§3Blocks in the Sky!"));
+        serverSelectInv.setItem(8, itemStackWithNameAndLore(Material.BEACON, "§1§lServer: §e§lCREATIVE","§3Create things?"));
+    }
+
+    GenericListener(HubPlugin hubPlugin) {
+        pl = hubPlugin;
+    }
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent pme){
@@ -39,14 +88,10 @@ public class GenericListener implements Listener {
         }
 
         if(p.isOnGround()){
-            if(flyingPlayers.contains(p)){
-                flyingPlayers.remove(p);
-            }
-            if(respawnedPlayers.contains(p)){
-                respawnedPlayers.remove(p);
-            }
+            flyingPlayers.remove(p.getUniqueId());
+            respawnedPlayers.remove(p.getUniqueId());
         }else{
-            if(respawnedPlayers.contains(p)) {
+            if(respawnedPlayers.contains(p.getUniqueId())) {
                 Vector v = p.getVelocity();
                 v.setX(0); v.setZ(0);
                 p.setVelocity(v);
@@ -55,9 +100,9 @@ public class GenericListener implements Listener {
                 nl.setDirection(p.getLocation().getDirection());
                 p.setFallDistance(999f);
                 p.teleport(nl);
-                respawnedPlayers.add(p);
-            }else if(p.isSneaking()&&!p.isFlying()&&!flyingPlayers.contains(p)&&pme.getTo().getY() > pme.getFrom().getY()){
-                flyingPlayers.add(p);
+                respawnedPlayers.add(p.getUniqueId());
+            }else if(p.isSneaking()&&!p.isFlying()&&!flyingPlayers.contains(p.getUniqueId())&&pme.getTo().getY() > pme.getFrom().getY()){
+                flyingPlayers.add(p.getUniqueId());
                 Vector v = p.getLocation().getDirection().normalize().multiply(.75);
                 v.setY(pme.getTo().getY() - pme.getFrom().getY());
                 p.setVelocity(v);
@@ -94,11 +139,78 @@ public class GenericListener implements Listener {
         Player p = pje.getPlayer();
         HubPlugin.ncsbt.addPlayer(p);
 
+        for (UUID u:playersHidingOthers) {
+            pl.getServer().getPlayer(u).hidePlayer(pl,p);
+        }
+
         if(!p.hasPermission("plscrafthub.alwaysspawnbypass")){
            p.teleport(new Location(p.getWorld(),0.5,110.5,0.5));
         }
+
+        p.getInventory().setContents(playerInv.getContents());
     }
 
+    @EventHandler (priority = EventPriority.HIGHEST)
+    public void onPlayerItemUse(PlayerInteractEvent pie){
+        if(pie.getAction() == Action.RIGHT_CLICK_AIR || pie.getAction() == Action.RIGHT_CLICK_BLOCK){
+            if(pie.getItem()==null) return;
+
+            if(pie.getItem().isSimilar(serverSelectItem)){
+                pie.setCancelled(true);
+
+                pie.getPlayer().openInventory(serverSelectInv);
+            }
+            if(pie.getItem().isSimilar(hidePlayersItem)){
+                Player p = pie.getPlayer();
+                playersHidingOthers.add(p.getUniqueId());
+
+                p.getInventory().setItem(TOGGLE_PLAYER_SLOT, showPlayersItem);
+                for (Player tp: pl.getServer().getOnlinePlayers()) {
+                    p.hidePlayer(pl, tp);
+                }
+            }
+            if(pie.getItem().isSimilar(showPlayersItem)){
+                Player p = pie.getPlayer();
+                playersHidingOthers.remove(p.getUniqueId());
+
+                p.getInventory().setItem(TOGGLE_PLAYER_SLOT, hidePlayersItem);
+                for (Player tp: pl.getServer().getOnlinePlayers()) {
+                    p.showPlayer(pl, tp);
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onInventoryDrag(InventoryDragEvent ide){
+        ide.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent ice){
+        if(!(ice.getWhoClicked() instanceof Player)) return;
+
+        if(ice.getInventory().getName().equals(serverSelectInv.getName())) {
+            ItemStack is = ice.getCurrentItem();
+            String itemName = ChatColor.stripColor(is.getItemMeta().getDisplayName());
+            System.out.println(itemName);
+            if(itemName.startsWith("Server: ")){
+                Player p = (Player)ice.getWhoClicked();
+                String serverName = itemName.substring(8).toLowerCase();
+
+                System.out.println("Sending " + p.getDisplayName() + " to " + serverName);
+
+                ByteArrayDataOutput out = ByteStreams.newDataOutput();
+                out.writeUTF("Connect");
+                out.writeUTF(serverName);
+                p.sendPluginMessage(pl,"BungeeCord",out.toByteArray());
+            }
+        }
+
+        ice.setCancelled(true);
+    }
+
+    @EventHandler
     public void onPlayerLeave(PlayerQuitEvent pqe){
         Player p = pqe.getPlayer();
         HubPlugin.ncsbt.removePlayer(p);
